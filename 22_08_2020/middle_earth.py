3cdb8bb82560189c7f7c8f02f70594fe770efe59
Joemx <jose.c.mondragon@gmail.com>
2020-08-22 13:29:15 -0500

    Update middle_earth.py
    Updated for multiple printing

--- 22_08_2020/middle_earth.py
+++ 22_08_2020/middle_earth.py
@@ -3,8 +3,8 @@ from random import random
 class Creature:
     def __init__(self, l = 100, m = 0 , f = 0):
         self.__life = l
-        self.__magic = 0
-        self.__force = 0
+        self.__magic = m
+        self.__force = f
         self.__magic_m = 0
         self.__force_m = 0
     def getStrength(self):
@@ -30,22 +30,37 @@ class Orc(Creature):
     def __init__(self):
         super().__init__(l=100,m=5+random()*5, f=12+random()*5)
 
+class Horde:
+    def __init__(self, creature):
+        self.__px = 0
+        self.__py = 0
+        self.__vx = 0
+        self.__vy = 0
+        self.__creatures = []
+        self.__creatures.append(creature)
+
+    def __str__(self):
+        cts = ' & '.join(map(str,self.__creatures))
+        cts = '{' + cts  + '}'
+        return f'x:{self.__px} y:{self.__py} vx:{self.__vx} vy:{self.__vy} c:{cts}'
 class Middle_Earth:
     def __init__(self):
-        self.orc = Orc()
-        self.elf = Elf()
+        self.elves_hordes = []
+        self.orcs_hordes = []
+
+        self.elves_hordes.append( Horde(Elf() )  )
+        self.orcs_hordes.append( Horde(Orc() ) )
+        self.orcs_hordes.append( Horde(Orc() ) )
 
     def update(self):
         # check possible battles
         a = 0
         #move positions of alive hordes
-
     def battle(hordeA, hordeB):
         a = 0
-
     def __str__(self):
-        return f'World! {self.orc} {self.elf} '
-
-
+        orcs = ' '.join(map(str , self.orcs_hordes ))
+        elves = ' '.join(map(str , self.elves_hordes ))
+        return f'World!\nOrcs Hordes:\n{orcs} \nElves hords:\n{elves} '
 world = Middle_Earth()
 print(world)
